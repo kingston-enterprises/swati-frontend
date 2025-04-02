19bf11c424d6e02c32ea1196f968ff54b1acc2b3
kingston-5 <qhawem54@gmail.com>
2025-04-02 14:03:58 +0200

    first commit local

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+      },
+      manifest: {
+        name: 'Eswatini Shop',
+        short_name: 'Eswatini',
+        description: 'Eswatini Number 1 Online shop',
+        theme_color: '#ffffff',
+        icons: [],
+      },
+    })
+
+    ],
+})
+
