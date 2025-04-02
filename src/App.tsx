0d57c3d7975f05699dd660db5a2e5aa897eba92b
kingston-5 <qhawem54@gmail.com>
2025-04-02 14:18:03 +0200

    SCRUM-3 front-end ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import './App.css'
 function App() {
   
   return (
-    <><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+    <>
+    <div className="min-h-screen w-screen bg-gray-900 flex flex-col items-center justify-center relative px-4">
+    <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center opacity-30"
+        style={{backgroundImage: "url('https://images.unsplash.com/photo-1604093882750-3ed498f3178b')"}}> */
+    </div>
+ 
+    <h1 className="text-5xl md:text-7xl text-white font-bold mb-8 z-10">Eswatini.shop</h1>
+  <h1 className="text-3xl md:text-5xl text-white  mb-8 z-10">Coming Soon</h1>
+    <p className="text-white text-xl md:text-2xl">
+        We're working hard to bring you something amazing. Stay tuned!
+    </p>
+</div>
     </>
   )
 }
